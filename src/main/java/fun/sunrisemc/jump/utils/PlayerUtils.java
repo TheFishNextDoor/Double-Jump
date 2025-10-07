00c1c4b1bc9ebad52d36b8f3f0473302e687571a
TheFishNextDoor <wonderfulwiz9@gmail.com>
2025-10-07 12:19:51 -0500

    fix: add nullability annotation to isOnGround method parameter

--- src/main/java/fun/sunrisemc/jump/utils/PlayerUtils.java
+++ src/main/java/fun/sunrisemc/jump/utils/PlayerUtils.java
@@ -2,10 +2,11 @@ package fun.sunrisemc.jump.utils;
 
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.checkerframework.checker.nullness.qual.NonNull;
 
 public class PlayerUtils {
 
-    public static boolean isOnGround(Player player) {
+    public static boolean isOnGround(@NonNull Player player) {
         Entity entity = (Entity) player;
         return entity.isOnGround();
     }
